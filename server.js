16f6dae87829ace7a1154f3408b3b038820f4eca
Nick Link <nlink2021@gmail.com>
2021-04-12 15:42:47 -0500

    launch frontend

--- server.js
+++ server.js
@@ -4,14 +4,25 @@ const app = express()
 const routesReport = require('rowdy-logger').begin(app)
 
 const path = require('path')
+const replaceInFile = require('replace-in-file')
 
 app.get('/', (req, res) => {
   const filepath = path.join(__dirname, 'index.html')
   res.sendFile(filepath)
 })
 
-app.get('/main.js', (req, res) => {
+app.get('/main.js', async (req, res) => {
   const filepath = path.join(__dirname, 'main.js')
+
+  if (process.env.NODE_ENV === 'production') {
+    await replaceInFile({
+      files: filepath,
+      from: 'http://localhost:3001',
+      to: 'https://film-favs.herokuapp.com'
+    })
+  }
+  
+
   res.sendFile(filepath)
 })
 
